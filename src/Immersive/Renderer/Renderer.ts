c1678fac7540bd035531a8f75ac48519c93ae0c1
Wannes Vanspranghe <90281974+ThirdAvenue@users.noreply.github.com>
2024-09-20 17:51:18 +0200

    Update Renderer.ts

--- src/Immersive/Renderer/Renderer.ts
+++ src/Immersive/Renderer/Renderer.ts
@@ -55,17 +55,22 @@ export class Renderer {
         //add statistics to the page
         this.stats = new Stats()
         if (OdinConfigurator.instance.debugMode === true) {
-           // document.body.appendChild(this.stats.dom)
+            // document.body.appendChild(this.stats.dom)
         }
     }
-    public async mount(container: HTMLDivElement,cameraSettings:Array<any>) {
+    public async mount(container: HTMLDivElement, cameraSettings: Array<any>) {
         this.container = container
         const { clientWidth, clientHeight } = this.container
         this.container.appendChild(this._renderer.domElement)
         this._renderer.setSize(clientWidth, clientHeight)
 
         console.log(cameraSettings)
-        await this.cameraSetup(clientWidth, clientHeight,cameraSettings)
+        const CamPos = new Vector3(cameraSettings[0].xCam, cameraSettings[0].yCam, cameraSettings[0].zCam)
+        const TargetPos = new Vector3(cameraSettings[0].xLook, cameraSettings[0].yLook, cameraSettings[0].zLook)
+        Renderer._camera = new OrbitCamera(20, this.canvas, 0.01, 100, CamPos, TargetPos)
+        Renderer._camera.aspect = clientWidth / clientHeight
+        Renderer._camera.updateProjectionMatrix()
+
         window.addEventListener('resize', this.onWindowResize)
         //provide own rendertarget to the composer 
         const renderTarget = new WebGLRenderTarget(
@@ -86,25 +91,25 @@ export class Renderer {
         this._scene.init()
         gui.add(this.scene.sunLight, 'intensity', 0, 2, 0.1).name('Sunlight intensity')
         gui.add(this.scene.hemisphericLight, 'intensity', 0, 2, 0.01).name('hemisphere intensity')
-        gui.add(Renderer._camera.position, 'x',-10,10,0.1).name("CposX")
-        gui.add(Renderer._camera.position, 'y',-10,10,0.1).name("CposY")
-        gui.add(Renderer._camera.position, 'z',-10,10,0.1).name("CposZ")
+        gui.add(Renderer._camera.position, 'x', -10, 10, 0.1).name("CposX")
+        gui.add(Renderer._camera.position, 'y', -10, 10, 0.1).name("CposY")
+        gui.add(Renderer._camera.position, 'z', -10, 10, 0.1).name("CposZ")
 
 
-        gui.add( this.composer.GTAOPass, 'output', {
+        gui.add(this.composer.GTAOPass, 'output', {
             'Default': GTAOPass.OUTPUT.Default,
             'Diffuse': GTAOPass.OUTPUT.Diffuse,
             'AO Only': GTAOPass.OUTPUT.AO,
             'AO Only + Denoise': GTAOPass.OUTPUT.Denoise,
             'Depth': GTAOPass.OUTPUT.Depth,
             'Normal': GTAOPass.OUTPUT.Normal
-        } ).onChange( function ( value ) {
+        }).onChange(function (value) {
 
             OdinConfigurator.instance.renderer.composer.GTAOPass.output = value;
 
-        } );
+        });
         const aoParameters = {
-            enable:true,
+            enable: true,
             radius: 1,
             distanceExponent: 3.73,
             thickness: 10,
@@ -116,35 +121,34 @@ export class Renderer {
         const pdParameters = {
             lumaPhi: 10.,
             depthPhi: 2.,
-            normalPhi:11.,
+            normalPhi: 11.,
             radius: 18,
             radiusExponent: 4.,
             rings: 2.,
             samples: 16,
         };
 
-        gui.add(aoParameters, "enable" ).onChange( () => this.composer.AOEnabled= aoParameters.enable  );
-        gui.add(this.composer.GTAOPass,'blendIntensity' ).min( 0 ).max( 1 ).step( 0.01 );
-        gui.add(aoParameters,'radius' ).min( 0.01 ).max( 1 ).step( 0.01 ).onChange( () => this.composer.GTAOPass.updateGtaoMaterial( aoParameters ) );
-        gui.add( aoParameters, 'distanceExponent' ).min( 1 ).max( 4 ).step( 0.01 ).onChange( () => this.composer.GTAOPass.updateGtaoMaterial( aoParameters ) );
-        gui.add( aoParameters, 'thickness' ).min( 0.01 ).max( 10 ).step( 0.01 ).onChange( () => this.composer.GTAOPass.updateGtaoMaterial( aoParameters ) );
-        gui.add( aoParameters, 'distanceFallOff' ).min( 0 ).max( 1 ).step( 0.01 ).onChange( () => this.composer.GTAOPass.updateGtaoMaterial( aoParameters ) );
-        gui.add( aoParameters, 'scale' ).min( 0.01 ).max( 2.0 ).step( 0.01 ).onChange( () => this.composer.GTAOPass.updateGtaoMaterial( aoParameters ) );
-        gui.add( aoParameters, 'samples' ).min( 2 ).max( 32 ).step( 1 ).onChange( () => this.composer.GTAOPass.updateGtaoMaterial( aoParameters ) );
-        gui.add( aoParameters, 'screenSpaceRadius' ).onChange( () => this.composer.GTAOPass.updateGtaoMaterial( aoParameters ) );
-        gui.add( pdParameters, 'lumaPhi' ).min( 0 ).max( 20 ).step( 0.01 ).onChange( () => this.composer.GTAOPass.updatePdMaterial( pdParameters ) );
-        gui.add( pdParameters, 'depthPhi' ).min( 0.01 ).max( 20 ).step( 0.01 ).onChange( () => this.composer.GTAOPass.updatePdMaterial( pdParameters ) );
-        gui.add( pdParameters, 'normalPhi' ).min( 0.01 ).max( 20 ).step( 0.01 ).onChange( () => this.composer.GTAOPass.updatePdMaterial( pdParameters ) );
-        gui.add( pdParameters, 'radius' ).min( 0 ).max( 32 ).step( 1 ).onChange( () => this.composer.GTAOPass.updatePdMaterial( pdParameters ) );
-        gui.add( pdParameters, 'radiusExponent' ).min( 0.1 ).max( 4. ).step( 0.1 ).onChange( () => this.composer.GTAOPass.updatePdMaterial( pdParameters ) );
-        gui.add( pdParameters, 'rings' ).min( 1 ).max( 16 ).step( 0.125 ).onChange( () => this.composer.GTAOPass.updatePdMaterial( pdParameters ) );
-        gui.add( pdParameters, 'samples' ).min( 2 ).max( 32 ).step( 1 ).onChange( () => this.composer.GTAOPass.updatePdMaterial( pdParameters ) );
+        gui.add(aoParameters, "enable").onChange(() => this.composer.AOEnabled = aoParameters.enable);
+        gui.add(this.composer.GTAOPass, 'blendIntensity').min(0).max(1).step(0.01);
+        gui.add(aoParameters, 'radius').min(0.01).max(1).step(0.01).onChange(() => this.composer.GTAOPass.updateGtaoMaterial(aoParameters));
+        gui.add(aoParameters, 'distanceExponent').min(1).max(4).step(0.01).onChange(() => this.composer.GTAOPass.updateGtaoMaterial(aoParameters));
+        gui.add(aoParameters, 'thickness').min(0.01).max(10).step(0.01).onChange(() => this.composer.GTAOPass.updateGtaoMaterial(aoParameters));
+        gui.add(aoParameters, 'distanceFallOff').min(0).max(1).step(0.01).onChange(() => this.composer.GTAOPass.updateGtaoMaterial(aoParameters));
+        gui.add(aoParameters, 'scale').min(0.01).max(2.0).step(0.01).onChange(() => this.composer.GTAOPass.updateGtaoMaterial(aoParameters));
+        gui.add(aoParameters, 'samples').min(2).max(32).step(1).onChange(() => this.composer.GTAOPass.updateGtaoMaterial(aoParameters));
+        gui.add(aoParameters, 'screenSpaceRadius').onChange(() => this.composer.GTAOPass.updateGtaoMaterial(aoParameters));
+        gui.add(pdParameters, 'lumaPhi').min(0).max(20).step(0.01).onChange(() => this.composer.GTAOPass.updatePdMaterial(pdParameters));
+        gui.add(pdParameters, 'depthPhi').min(0.01).max(20).step(0.01).onChange(() => this.composer.GTAOPass.updatePdMaterial(pdParameters));
+        gui.add(pdParameters, 'normalPhi').min(0.01).max(20).step(0.01).onChange(() => this.composer.GTAOPass.updatePdMaterial(pdParameters));
+        gui.add(pdParameters, 'radius').min(0).max(32).step(1).onChange(() => this.composer.GTAOPass.updatePdMaterial(pdParameters));
+        gui.add(pdParameters, 'radiusExponent').min(0.1).max(4.).step(0.1).onChange(() => this.composer.GTAOPass.updatePdMaterial(pdParameters));
+        gui.add(pdParameters, 'rings').min(1).max(16).step(0.125).onChange(() => this.composer.GTAOPass.updatePdMaterial(pdParameters));
+        gui.add(pdParameters, 'samples').min(2).max(32).step(1).onChange(() => this.composer.GTAOPass.updatePdMaterial(pdParameters));
 
     }
     public onWindowResize() {
         const clientWidth = OdinConfigurator.instance.canvas.clientWidth
         const clientHeight = OdinConfigurator.instance.canvas.clientHeight
-        console.log(clientWidth, clientHeight)
         OdinConfigurator.instance.renderer._renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
         OdinConfigurator.instance.renderer._renderer.setSize(clientWidth, clientHeight)
         Renderer._camera.aspect = clientWidth / clientHeight;
@@ -153,37 +157,7 @@ export class Renderer {
         OdinConfigurator.instance.renderer.composer.setSize(clientWidth, clientHeight)
 
     }
-    private async cameraSetup(clientWidth: number, clientHeight: number,cameraSettings:Array<any>) {
-        debugger
-/*  
-        if (OdinConfigurator.instance.productAssembler) {
-            const camera = await OdinConfigurator.instance.productAssembler.object.traverse((child) => {
-                if (child instanceof Camera) {
-                    return child
-                }
-            })
-            if (camera != null) {
-                Renderer._camera = camera
-                Renderer._camera.updateProjectionMatrix()
-
-            }
-        } */
-        const CamPos = new Vector3(cameraSettings[0].xCam, cameraSettings[0].yCam, cameraSettings[0].zCam)
-        const TargetPos = new Vector3(cameraSettings[0].xLook, cameraSettings[0].yLook, cameraSettings[0].zLook)
-
-        if (Renderer._camera == null) {
-            alert('No camera found')
-            Renderer._camera = new OrbitCamera(20, this.canvas, 0.01, 100,CamPos,TargetPos)
-            if (Renderer._camera instanceof OrbitCamera) {
-                alert(clientHeight)
-                alert(clientWidth)
-                Renderer._camera.aspect = clientWidth / clientHeight
-                Renderer._camera.updateProjectionMatrix()
-            }
-        }
-
-
-    }
+    
     private render() {
         if (Renderer._camera instanceof OrbitCamera) {
             Renderer._camera.controls.update();
